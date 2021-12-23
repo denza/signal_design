afcfc4001f5ddbd044e3fc3dd803459d7bfbcbea
Nihad Obralić <nihad.obralic@gmail.com>
2021-12-23 01:34:06 +0100

    apexchart init

--- src/index.js
+++ src/index.js
@@ -4,6 +4,28 @@ import '@/styles/tailwind.scss'
 // Import Alpine.js
 import Alpine from 'alpinejs'
 
+// Import ApexCharts
+import ApexCharts from 'apexcharts'
+
 window.Alpine = Alpine
 
 Alpine.start()
+
+var options = {
+  chart: {
+    type: 'line',
+  },
+  series: [
+    {
+      name: 'sales',
+      data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+    },
+  ],
+  xaxis: {
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+  },
+}
+
+var chart = new ApexCharts(document.querySelector('#chart01'), options)
+
+chart.render()
