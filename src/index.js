9298d047ffaf64507250a0200a4819fd983842b0
Nihad Obralić <nihad.obralic@gmail.com>
2021-12-24 04:30:55 +0100

    update charts

--- src/index.js
+++ src/index.js
@@ -146,13 +146,26 @@ let chart_global_options = {
       )
     },
   },
+  noData: {
+    text: 'Loading...',
+    align: 'center',
+    verticalAlign: 'middle',
+    offsetX: 0,
+    offsetY: 0,
+    style: {
+      color: '#000000',
+      fontSize: '14px',
+      fontFamily: 'Helvetica',
+    },
+  },
 }
 Object.assign(Apex, chart_global_options)
 
+// s1 chart
 let s1_chart01_day = {
   chart: {
     id: 's1_chart01',
-    group: 's1',
+    group: 'n1',
     type: 'line',
   },
   series: [
@@ -166,10 +179,10 @@ let s1_chart01_day = {
         { x: '06:00', y: '-22' },
         { x: '08:00', y: '-10' },
         { x: '10:00', y: '-24' },
-        { x: '11:15', y: '5', strokeColor: '#C8233380', fillColor: '#C82333' },
+        { x: '11:15', y: '5,05', strokeColor: '#C8233380', fillColor: '#C82333' },
         { x: '11:32', y: '-20' },
         { x: '12:00', y: '-5', strokeColor: '#C8233380', fillColor: '#C82333' },
-        { x: '14:00', y: '-25' },
+        { x: '14:00', y: '-25,25' },
         { x: '16:00', y: '-19' },
       ],
     },
@@ -239,13 +252,13 @@ let s1_chart01_day = {
     ],
   },
 }
-let s1_chart = new ApexCharts(document.querySelector('#s1_chart01'), s1_chart01_day)
-s1_chart.render()
+let s1_1_chart = new ApexCharts(document.querySelector('#s1_chart01'), s1_chart01_day)
+s1_1_chart.render()
 
 let s1_chart02_week = {
   chart: {
     id: 's1_chart02',
-    group: 's2',
+    group: 'n2-4',
     type: 'line',
   },
   series: [
@@ -263,13 +276,13 @@ let s1_chart02_week = {
     },
   ],
 }
-let s2_chart = new ApexCharts(document.querySelector('#s1_chart02'), s1_chart02_week)
-s2_chart.render()
+let s1_2_chart = new ApexCharts(document.querySelector('#s1_chart02'), s1_chart02_week)
+s1_2_chart.render()
 
 let s1_chart03_month = {
   chart: {
     id: 's1_chart02',
-    group: 's3',
+    group: 'n3-1',
     type: 'line',
   },
   series: [
@@ -284,5 +297,275 @@ let s1_chart03_month = {
     },
   ],
 }
-let s3_chart = new ApexCharts(document.querySelector('#s1_chart03'), s1_chart03_month)
-s3_chart.render()
+let s1_3_chart = new ApexCharts(document.querySelector('#s1_chart03'), s1_chart03_month)
+s1_3_chart.render()
+
+// s2chart
+let s2_chart01_day = {
+  chart: {
+    id: 's2_chart01',
+    group: 'n1-1',
+    type: 'line',
+  },
+  series: [
+    {
+      name: 'Temperature',
+      data: [
+        { x: '00:01', y: '-20' },
+        { x: '01:00', y: '-20' },
+        { x: '02:00', y: '-22' },
+        { x: '04:00', y: '-25' },
+        { x: '06:00', y: '-22' },
+        { x: '08:00', y: '-26' },
+        { x: '10:00', y: '-20' },
+        { x: '11:15', y: '-15' },
+        { x: '11:32', y: '-19' },
+        { x: '12:00', y: '-28' },
+        { x: '14:00', y: '-22.8' },
+        { x: '16:00', y: '-25' },
+      ],
+    },
+  ],
+  annotations: {
+    points: [],
+  },
+}
+let s2_1_chart = new ApexCharts(document.querySelector('#s2_chart01'), s2_chart01_day)
+s2_1_chart.render()
+
+let s2_chart02_week = {
+  chart: {
+    id: 's2_chart02',
+    group: 'n2-1',
+    type: 'line',
+  },
+  series: [
+    {
+      name: 'Temperature',
+      data: [
+        { x: 'Monday', y: '-24' },
+        { x: 'Tuesday', y: '-29' },
+        { x: 'Wednesday', y: '-4' },
+        { x: 'Thursday', y: '-10' },
+        { x: 'Friday', y: '-5' },
+        { x: 'Saturday', y: '-1' },
+        { x: 'Sunday', y: '-7' },
+      ],
+    },
+  ],
+}
+let s2_2_chart = new ApexCharts(document.querySelector('#s2_chart02'), s2_chart02_week)
+s2_2_chart.render()
+
+let s2_chart03_month = {
+  chart: {
+    id: 's2_chart02',
+    group: 'n3-2',
+    type: 'line',
+  },
+  series: [
+    {
+      name: 'Temperature',
+      data: [
+        { x: 'Week 1', y: '0' },
+        { x: 'Week 2', y: '-16' },
+        { x: 'Week 3', y: '-7' },
+        { x: 'Week 4', y: '-9' },
+      ],
+    },
+  ],
+}
+let s2_3_chart = new ApexCharts(document.querySelector('#s2_chart03'), s2_chart03_month)
+s2_3_chart.render()
+
+// s3 chart
+let s3_chart01_day = {
+  chart: {
+    id: 's3_chart01',
+    group: 'n1-2',
+    type: 'line',
+  },
+  series: [
+    {
+      name: 'Temperature',
+      data: [
+        { x: '00:00', y: '-33' },
+        { x: '02:00', y: '-32' },
+        { x: '04:00', y: '-33' },
+        { x: '06:00', y: '-32' },
+        { x: '08:00', y: '-33' },
+        { x: '10:00', y: '-34' },
+        { x: '12:00', y: '-33' },
+        { x: '14:00', y: '5', strokeColor: '#C8233380', fillColor: '#C82333' },
+        { x: '16:00', y: '-5' },
+      ],
+    },
+  ],
+  annotations: {
+    points: [
+      {
+        x: '14:00',
+        y: 5,
+        marker: {
+          size: 16,
+          fillColor: '#C82333',
+          strokeColor: '#ffffff80',
+          strokeWidth: 8,
+          radius: 100,
+          cssClass: 'issue-img animate-pulse',
+        },
+        image: {
+          path: '/images/graph/fix.png',
+          width: 20,
+          height: 20,
+          offsetX: 0,
+          offsetY: 26,
+          appendTo: '.issue-img',
+        },
+      },
+    ],
+  },
+}
+let s3_1_chart = new ApexCharts(document.querySelector('#s3_chart01'), s3_chart01_day)
+s3_1_chart.render()
+
+let s3_chart02_week = {
+  chart: {
+    id: 's3_chart02',
+    group: 'n2-2',
+    type: 'line',
+  },
+  series: [
+    {
+      name: 'Temperature',
+      data: [
+        { x: 'Monday', y: '-24' },
+        { x: 'Tuesday', y: '-29' },
+        { x: 'Wednesday', y: '-4' },
+        { x: 'Thursday', y: '-10' },
+        { x: 'Friday', y: '-5' },
+        { x: 'Saturday', y: '-1' },
+        { x: 'Sunday', y: '-7' },
+      ],
+    },
+  ],
+}
+let s3_2_chart = new ApexCharts(document.querySelector('#s3_chart02'), s3_chart02_week)
+s3_2_chart.render()
+
+let s3_chart03_month = {
+  chart: {
+    id: 's3_chart02',
+    group: 'n3-3',
+    type: 'line',
+  },
+  series: [
+    {
+      name: 'Temperature',
+      data: [
+        { x: 'Week 1', y: '0' },
+        { x: 'Week 2', y: '-16' },
+        { x: 'Week 3', y: '-7' },
+        { x: 'Week 4', y: '-9' },
+      ],
+    },
+  ],
+}
+let s3_3_chart = new ApexCharts(document.querySelector('#s3_chart03'), s3_chart03_month)
+s3_3_chart.render()
+
+// s4 chart
+let s4_chart01_day = {
+  chart: {
+    id: 's4_chart01',
+    group: 'n1-3',
+    type: 'line',
+  },
+  series: [
+    {
+      name: 'Temperature',
+      data: [
+        { x: '00:00', y: '-15' },
+        { x: '02:00', y: '-14' },
+        { x: '04:00', y: '-22' },
+        { x: '06:00', y: '-10' },
+        { x: '08:00', y: '-24' },
+        { x: '10:00', y: '-12' },
+        { x: '12:00', y: '-20' },
+        { x: '14:00', y: '5', strokeColor: '#C8233380', fillColor: '#C82333' },
+        { x: '16:00', y: '-25' },
+      ],
+    },
+  ],
+  annotations: {
+    points: [
+      {
+        x: '14:00',
+        y: 5,
+        marker: {
+          size: 16,
+          fillColor: '#C82333',
+          strokeColor: '#ffffff80',
+          strokeWidth: 8,
+          radius: 100,
+          cssClass: 'issue-img animate-pulse',
+        },
+        image: {
+          path: '/images/graph/opened_door.png',
+          width: 20,
+          height: 20,
+          offsetX: 0,
+          offsetY: 26,
+          appendTo: '.issue-img',
+        },
+      },
+    ],
+  },
+}
+let s4_1_chart = new ApexCharts(document.querySelector('#s4_chart01'), s4_chart01_day)
+s4_1_chart.render()
+
+let s4_chart02_week = {
+  chart: {
+    id: 's4_chart02',
+    group: 'n2-3',
+    type: 'line',
+  },
+  series: [
+    {
+      name: 'Temperature',
+      data: [
+        { x: 'Monday', y: '-24' },
+        { x: 'Tuesday', y: '-29' },
+        { x: 'Wednesday', y: '-4' },
+        { x: 'Thursday', y: '-10' },
+        { x: 'Friday', y: '-5' },
+        { x: 'Saturday', y: '-1' },
+        { x: 'Sunday', y: '-7' },
+      ],
+    },
+  ],
+}
+let s4_2_chart = new ApexCharts(document.querySelector('#s4_chart02'), s4_chart02_week)
+s4_2_chart.render()
+
+let s4_chart03_month = {
+  chart: {
+    id: 's4_chart02',
+    group: 'n3-4',
+    type: 'line',
+  },
+  series: [
+    {
+      name: 'Temperature',
+      data: [
+        { x: 'Week 1', y: '0' },
+        { x: 'Week 2', y: '-16' },
+        { x: 'Week 3', y: '-7' },
+        { x: 'Week 4', y: '-9' },
+      ],
+    },
+  ],
+}
+let s4_3_chart = new ApexCharts(document.querySelector('#s4_chart03'), s4_chart03_month)
+s4_3_chart.render()
