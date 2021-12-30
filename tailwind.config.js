51440f4f351f03fdb5e07bea22b70084ca2e7441
Nihad Obralić <nihad.obralic@gmail.com>
2021-12-30 13:23:08 +0100

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -466,6 +466,7 @@ module.exports = {
       '5/6': '83.333333%',
       full: '100%',
       screen: '100vh',
+      halfscreen: '50vh',
     }),
     inset: (theme, { negative }) => ({
       auto: 'auto',
